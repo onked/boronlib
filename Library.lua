74b0fc4651fd2d9fa25c198a58ec53f481b754e2
dekno <55319846+onked@users.noreply.github.com>
2025-01-09 15:29:09 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -183,9 +183,9 @@ function Library:MakeDraggable(Instance, Cutoff)
 			while UIS:IsMouseButtonPressed(Enum.UserInputType.MouseButton1) do
 				Instance.Position = UDim2.new(
 					0,
-					Mouse.X - ObjPos.X,
+					Mouse.X - ObjPos.X + (Instance.Size.X.Offset * Instance.AnchorPoint.X),
 					0,
-					Mouse.Y - ObjPos.Y
+					Mouse.Y - ObjPos.Y + (Instance.Size.Y.Offset * Instance.AnchorPoint.Y)
 				);
 
 				RenderStepped:Wait()
